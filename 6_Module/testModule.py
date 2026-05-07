0b70385b709b3361131d9c620b84b58a56ac8af2
Gunter Werner <gunter.werner@gmx.de>
2026-05-07 13:29:31 +0200

    module change

---
+++ 6_Module/testModule.py
@@ -0,0 +1,6 @@
+#from "6_Module".myModule import begruessung, begruessen_mit_alter
+from Module.myModule import begruessung, begruessen_mit_alter
+
+print(begruessung("Charlie"))
+
+print(begruessen_mit_alter("Diana", 28))
--- testModule.py
+++
@@ -1,4 +0,0 @@
-from  Module1.myModule import begruessung, begruessen_mit_alter
-print(begruessung("Charlie"))
-
-print(begruessen_mit_alter("Diana", 28))
